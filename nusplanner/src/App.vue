632dfa418a159cb4e5008d9ec689f3e46b73093a
Weisheng97 <c.weisheng97@gmail.com>
2020-02-25 11:59:25 +0800

    Adding module search component

--- nusplanner/src/App.vue
+++ nusplanner/src/App.vue
@@ -1,18 +1,36 @@
 <template>
   <div id="app">
     <app-header></app-header>
+    <moduleAdding></moduleAdding>
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
+import Vue from "vue";
+import { AutoCompletePlugin } from "@syncfusion/ej2-vue-dropdowns";
+import Header from './components/Header.vue';
+import ModuleAdding from './components/ModuleAdding.vue';
 
-export default {
-  components: {
+Vue.use(AutoCompletePlugin);
+export default Vue.extend({
+  data: function() {
+    return {
+      dataItem:[
+        { Id: 'Module1', Module: 'BT1101'},
+        { Id: 'Module2', Module: 'BT2101'},
+        { Id: 'Module3', Module: 'BT2102'},
+        { Id: 'Module4', Module: 'BT3102'},
+        { Id: 'Module5', Module: 'BT2103'},
+      ],
+      dataFields:{value: 'Module'}
+    }
+  },
+    components: {
     //Registering with a name
     'app-header':Header,
+    'ModuleAdding':ModuleAdding,
   }
-}
+})
 </script>
 
 <style>
@@ -24,6 +42,7 @@ export default {
   color: #2c3e50;
   
 }
+@import url(https://cdn.syncfusion.com/ej2/material.css);
 /* For the headers to look good, be sure to reset the margin and padding of the body */
 body {
 	margin:0;
