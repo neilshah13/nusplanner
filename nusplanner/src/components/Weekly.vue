fafaa4789c9f2bfa1a9ea31fa08752a82f5d2877
Gerald Tan <tanjunweigerald1997@gmail.com>
2020-03-17 00:42:43 +0800

    Add group members feature

--- nusplanner/src/components/Weekly.vue
+++ nusplanner/src/components/Weekly.vue
@@ -53,9 +53,9 @@
       </v-sheet>
 
 <!-- add GroupMembers dialog -->
-<v-dialog v-model="groupMembers" fullscreen max-width="550">
+<v-dialog v-model="groupMembers" max-width="550">
 
-  <v-card>
+  <v-card color="blue-grey" >
     <v-card-title primary-title class="justify-center">
       <v-row align = "left">
       <v-btn icon dark @click="groupMembers = false" color="warning" outlined> <!-- closing button -->
@@ -64,10 +64,75 @@
       </v-row>
     </v-card-title>
 
-    <v-card-text class='menu'> Choose Module :
+      <!-- Form start here ******************************-->
+      <div id="app">
+  <v-app id="inspire">
+    <v-card
+      color="blue-grey darken-1"
+      dark
+      :loading="isUpdating"
+    >
+      <template v-slot:progress>
+        <v-progress-linear
+          absolute
+          color="green lighten-3"
+          height="4"
+          indeterminate
+        ></v-progress-linear>``
+      </template>
+      <v-img
+        height="200"
+        src="../..//public/teamwork.jpg"
+      >
+        <v-row>
+          <v-col
+            class="text-right"
+            cols="12"
+          >
+            <v-menu
+              bottom
+              left
+              transition="slide-y-transition"
+            >
+              <template v-slot:activator="{ on }">
+                <v-btn
+                  icon
+                  v-on="on"
+                >
+                  <v-icon>mdi-dots-vertical</v-icon>
+                </v-btn>
+              </template>
+              <v-list>
+                <v-list-item @click="isUpdating = true">
+                  <v-list-item-action>
+                    <v-icon>mdi-settings</v-icon>
+                  </v-list-item-action>
+                  <v-list-item-content>
+                    <v-list-item-title>Update</v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+          </v-col>
+          <v-row
+            class="pa-4"
+            align="center"
+            justify="center"
+          >
+            <v-col class="text-center">
+              <h3 class="headline">{{ name }}</h3>
+            </v-col>
+          </v-row>
+        </v-row>
+      </v-img>
+      <v-form>
+        <v-container>
+          <v-row>
+
+              <v-card-text class='menu' > Choose Module :
       <v-menu>
         <template v-slot:activator="{ on }">
-        <v-btn outlined color='grey darken-2' v-on="on" class="btn">
+        <v-btn outlined v-on="on" class="btn" dark filled>
           <span>{{ modules[module] }}</span>
            <v-icon bottom>mdi-menu-down</v-icon>
           </v-btn>
@@ -94,7 +159,71 @@
             </v-list>
       </v-menu>
     </v-card-text>
-    <Form></Form>
+
+            <v-col
+              cols="12"
+            >
+              <v-text-field
+                class="groupname"
+                v-model= "name"
+                :disabled="isUpdating"
+                outlined
+                color="blue-grey lighten-2"
+                label="Group Name"
+              ></v-text-field>
+            </v-col>
+            <v-col
+              cols="12"
+              md="6"
+            >
+            </v-col>
+            <v-col cols="12">
+              <v-autocomplete
+                class = "groupmembers"
+                v-model="friends"
+                :disabled="isUpdating"
+                :items="people"
+                filled
+                dense
+                chips
+                color="blue-grey lighten-2"
+                label="Select"
+                item-text="name"
+                item-value="name"
+                multiple
+              >
+                <template v-slot:selection="data" >
+                  <v-chip
+                    v-bind="data.attrs"
+                    :input-value="data.selected"
+                    close
+                    @click="data.select"
+                    @click:close="remove(data.item)"
+                  >
+                    {{ data.item.name }}
+                  </v-chip>
+                </template>
+                <template v-slot:item="data">
+                  <template v-if="typeof data.item !== 'object'">
+                    <v-list-item-content v-text="data.item"></v-list-item-content>
+                  </template>
+                  <template v-else>
+                    <v-list-item-content>
+                      <v-list-item-title v-html="data.item.name"></v-list-item-title>
+                      <v-list-item-subtitle v-html="data.item.group"></v-list-item-subtitle>
+                    </v-list-item-content>
+                  </template>
+                </template>
+              </v-autocomplete>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-form>
+    </v-card>
+  </v-app>
+</div>
+                
+      <!-- Form end here ******************************-->
     </v-card>
 </v-dialog>
 
@@ -280,12 +409,10 @@
 </template>
 
 <script>
-import Form from "./Form.vue"
 import Swatches from 'vue-swatches'
 
 export default {
         components:{
-        Form,
         Swatches
       },
     data: () => ({
@@ -304,6 +431,34 @@ export default {
       dialog: false, // for adding event
       groupMembers: false,
       group: 'teamwork',
+
+    /* for the group project button */
+    autoUpdate: true,
+      friends: ['Gerald Tan', 'Neil Shah'],
+      isUpdating: false,
+      name: 'Teamwork Makes the Dream Work!',
+      people: [
+        { header: 'Users' },
+        { divider: true },
+        { name: 'Gerald Tan'},
+        { divider: true },
+        { name: 'Neil Shah' },
+        { divider: true },
+        { name: 'Wei Sheng' },
+        { divider: true },
+        { name: 'Koh Min' },
+        { divider: true },
+        { name: 'Carine'},
+        { divider: true },
+        { name: 'Xue Hui'},
+        { divider: true },
+        { name: 'Phillip'},
+        { divider: true },
+        { name: 'Holgate'},
+        { divider: true },
+      ],
+    /* end group project button */
+
       typeToLabel: {
         month: 'Month',
         week: 'Week',
@@ -378,6 +533,17 @@ export default {
     mounted () {
       this.getEvents()
     },
+
+    /* For group members button */
+    watch: {
+      isUpdating (val) {
+        if (val) {
+          setTimeout(() => (this.isUpdating = false), 3000)
+        }
+      },
+    },
+    /* End For group members button */
+
     computed: {
       title () {
         const { start, end } = this
@@ -409,6 +575,12 @@ export default {
       }
     },
     methods: {
+      /* For group members button */
+      remove (item) {
+        const index = this.friends.indexOf(item.name)
+        if (index >= 0) this.friends.splice(index, 1)
+      },
+      /* End For group members button */
       viewDay({ date }) {
         this.focus = date;
         this.type = "day"; // by default is month
@@ -506,6 +678,10 @@ export default {
 </script>
 
 <style scoped>
+  .groupname >>> .v-text-field__slot input {
+    color:black
+  }
+
 .calendar {
   margin: auto;
   max-height: 600px;
