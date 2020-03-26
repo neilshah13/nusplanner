eaecba6b2684f673f7aed8884b2aa5111249b12c
Gerald Tan <tanjunweigerald1997@gmail.com>
2020-03-26 19:56:38 +0800

    Edit and Delete Calendar Events'

--- nusplanner/src/components/CreateEvent.vue
+++ nusplanner/src/components/CreateEvent.vue
@@ -27,13 +27,13 @@
         </v-menu>
       </v-toolbar-title>
 
-      <v-form v-if="eventType == 'event'" @submit.prevent="addEvent" ref="form" class="neweventform">
+      <v-form v-if="eventType == 'event'" @submit.prevent="addEvent" ref="form" class="neweventform"> <!-- This submit dosent work tho, the submit links to the v-button submit below -->
         <v-text-field class= 'neweventfield' v-model="name" type="text" label="Event Name"></v-text-field>
         <v-text-field class= 'neweventfield' v-model="details" type="text" label="Details (e.g. Meet at Jurong East MRT)"></v-text-field>
-        <v-text-field class= 'neweventfield' v-model="start" type="date" label="Start Date"></v-text-field>
-        <v-text-field class= 'neweventfield' v-model="end" type="date" label="End Date"></v-text-field>
-        <v-text-field class= 'neweventfield' v-model="start" type="time" label="Start Time"></v-text-field>
-        <v-text-field class= 'neweventfield' v-model="end" type="time" label="End Time"></v-text-field>
+        <v-text-field class= 'neweventfield' v-model="startdate" type="date" label="Start Date"></v-text-field>
+        <v-text-field class= 'neweventfield' v-model="enddate" type="date" label="End Date"></v-text-field>
+        <v-text-field class= 'neweventfield' v-model="starttime" type="time" label="Start Time (Optional)"></v-text-field>
+        <v-text-field class= 'neweventfield' v-model="endtime" type="time" label="End Time (Optional)"></v-text-field>
                 <div class='colorfieldtitle'>
           <div class="mr-4">
           Please choose a color:
@@ -118,6 +118,7 @@
 </template>
 
 <script>
+import firebase from "firebase";
 import Swatches from 'vue-swatches'
 export default {
     components:{
@@ -128,11 +129,16 @@ export default {
       focus: '',
       color: '#1976D2', // default event color
       colors: ['#1976D2', 'red', 'pink', 'cyan', 'orange', 'indigo', 'purple'],
-      start: null,
-      end: null,
+      startdate: null,
+      enddate: null,
+      starttime: '',
+      endtime: '',
       snackbar: false,
+      details: '',
+      name: '',
       eventType: 'event', //default
       group: 'teamwork',
+
       eventTypes: {
         assignment: 'Assignment',
         event: 'Event',
@@ -153,7 +159,49 @@ export default {
       submittedEvent() {
         this.$emit('update-eventsnack')
         this.$emit('update-dialog')
+        this.addEvent()
+        this.$emit('getEventsfromDatabase') //calls the getEvent from Weekly.vue to update the calendar
       },
+       async addEvent () {
+        if (this.name && this.startdate && this.enddate) { //Takes in as long as there is a date
+          if (this.starttime != ''){ //If there is time input, then we input both date and time into database
+            var startinput = this.startdate.concat(" ".concat(this.starttime))
+            var endinput = this.enddate.concat(" ".concat(this.endtime))
+          } else { //if no time input, we just input date into database
+            startinput = this.startdate
+            endinput = this.enddate
+          }
+          var user = firebase.auth().currentUser;
+          //var eventAdded = 
+          await firebase.firestore().collection('event').add({
+            name: this.name,
+            details: this.details,
+            startdate:this.startdate,
+            enddate:this.enddate,
+            starttime: this.starttime,
+            endtime:this.endtime,
+            start: startinput,
+            end: endinput,
+            color: this.color,
+            global: false,
+            group_id: "fofcnxjlqwf", //change this
+            module_id: "jowfnflasdf", //change this
+            type: 1, //change this(?)
+            uid: user.uid, //change this
+          })
+          //console.log("before adding into user eventlist")
+          //console.log(firebase.firestore().collection('users').doc(user.uid).data().event_list)
+          //console.log("after adding into user eventlist")
+          this.name = '',
+          this.details = '',
+          this.startdate = '',
+          this.enddate = '',
+          this.starttime = '',
+          this.endtime = ''
+        } else {
+          alert('You must enter event name, start, and end time')
+        }
+        },
     },
     // model: {
     //   prop: 'dialog',
