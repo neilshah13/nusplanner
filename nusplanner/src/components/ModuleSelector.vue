127d2087545bb72381911bd1edcc186a21232eee
Weisheng97 <c.weisheng97@gmail.com>
2020-04-21 22:29:35 +0800

    asda

--- nusplanner/src/components/ModuleSelector.vue
+++ nusplanner/src/components/ModuleSelector.vue
@@ -6,7 +6,7 @@
       src="https://lh3.googleusercontent.com/proxy/6_4hiPG1zpcr-h5C4h8M0pGIqQYxp1hUCoWJXaf_E2gk_MmGWsMtzNHPPBuYg_PdxPnK4DR5Cdm8AoaWa4UiXZdOThEJZDoXXSECzyFHedCLWdgTWTVLpMCGhBQ4LuLeM6_0IoXcYZxsqMTmsa5R"
       dense
     >
-      <v-toolbar-title class="col-sm-2"> Add a Module! &nbsp;</v-toolbar-title><v-autocomplete
+      <v-toolbar-title class="col-sm-0">  &nbsp;</v-toolbar-title><v-autocomplete
       v-model="select"
       :loading="loading"
       :items="items"
@@ -16,7 +16,8 @@
       flat
       hide-no-data
       hide-details
-      label: append-icon = mdi-magnify
+      label = "Add a Module"
+      label:  append-icon = mdi-magnify
       solo-inverted
       dense
       ></v-autocomplete>
